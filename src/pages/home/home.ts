2e3c9f4b9ec65b8a09b8fc8c0d35d9d5137055a4
vitorizkiimanda <vitorizkiimanda@gmail.com>
2018-01-20 13:55:52 +0700

    home page Terbaru done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,38 +6,41 @@ import { NavController,Slides } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+  // @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
 
-  SwipedTabsIndicator :any= null;
-  tabs:any=[];
+  // SwipedTabsIndicator :any= null;
+  // tabs:any=[];
+
+  
+  pages: string = "terbaru";
 
  
   constructor(public navCtrl: NavController) {
-  	this.tabs=["page1","page2"];
+  	// this.tabs=["page1","page2"];
   }
   ionViewDidEnter() {
-    this.SwipedTabsIndicator = document.getElementById("indicator");
+    // this.SwipedTabsIndicator = document.getElementById("indicator");
   }
 
-  selectTab(index) {    
-    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
-    this.SwipedTabsSlider.slideTo(index, 500);
-  }
+  // selectTab(index) {    
+  //   this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+  //   this.SwipedTabsSlider.slideTo(index, 500);
+  // }
 
-  updateIndicatorPosition() {
-      // this condition is to avoid passing to incorrect index
-  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex() && this.SwipedTabsSlider.getActiveIndex() <1 )
-  	{
-      console.log(this.SwipedTabsSlider.length());
-  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
-    }
+  // updateIndicatorPosition() {
+  //     // this condition is to avoid passing to incorrect index
+  // 	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex() && this.SwipedTabsSlider.getActiveIndex() <1 )
+  // 	{
+  //     console.log(this.SwipedTabsSlider.length());
+  // 		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+  //   }
     
-    }
+  //   }
 
-  animateIndicator($event) {
-  	if(this.SwipedTabsIndicator)
-   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
-  }
+  // animateIndicator($event) {
+  // 	if(this.SwipedTabsIndicator)
+  //  	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
+  // }
 
 
 }
